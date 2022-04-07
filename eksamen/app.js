f5b5efb0f44a9cefe2a822b6688cc42f171a9c09
Retre4t <antonandersen98@yahoo.dk>
2022-04-07 10:04:57 +0200

    Update app.js

--- eksamen/app.js
+++ eksamen/app.js
@@ -15,58 +15,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./public")));
 
 
-
-//SQL connection 
-//npm install tedious
-var Connection = require("tedious").Connection;
-
-//vi henter metoden Request, for at lave requests til sql server
-var Request = require("tedious").Request;
-
-
-//vi henter config filen
-const config = require("./config.json");
-const { response } = require("./config.json");
-
-//vi sætter Connect og config sammen
-var connection = new Connection(config);
-
-//callback function som bliver triggered på err
-connection.on("connect", function(err){
-    if (err){
-        console.log(err)
-    } else {
-        console.log("connected to azure sql server");
-        const response = executeSQL();
-        console.log(response)
-    }
-});
-
-//vi kalder function, som trigger .on function
-connection.connect();
-
-function executeSQL(){
-
-    //Query her
-    request = new Request("select * from category", function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-
-    connection.execSql(request)
-    var counter = 1
-    SGLresponse = {}
-    request.on('row', function(columns){
-        SGLresponse[counter] = {}
-        columns.forEach(function(column){
-            SGLresponse[counter][column.metadata.colName] = column.value
-        });
-        counter += 1
-    });
-    return SGLresponse
-};
-
 //when signup on the signup page is clicked on the client side the server will receive the data from the client and save it to the database
 app.post("/signup", async (req, res) => {
   try {
