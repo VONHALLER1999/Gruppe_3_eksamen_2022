46346d8af9c36a239ef2093a252a33bded409eaf
VONHALLER1999 <62394387+VONHALLER1999@users.noreply.github.com>
2022-04-10 20:14:44 +0200

    pretty posts
    All post knap på index.html fylder nu en tabel ud med alle post data i databasen

--- eksamen/public/opslag.js
+++ eksamen/public/opslag.js
@@ -1,37 +1,44 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+
   document.getElementById("postBtn").addEventListener("click", (event) => {
     event.preventDefault();
     console.log("clicked post");
     fetch("http://localhost:1010/allposts")
       .then((res) => res.json())
       .then(function (result) {
-        appendData(result);
+        createTable(result);
+      })
+      .then(function() { 
+        console.log("success") 
       })
       .catch(function (err) {
         console.log(err);
       });
 
-    function appendData(result) {
-      var posts = "";
-      for (var i = 0; i < result.recordset.length; i++) {
-        posts +=
-          result.recordset[i].email +
-          " " +
-          result.recordset[i].postal +
-          " " +
-          result.recordset[i].category_name +
-          " " +
-          result.recordset[i].created_at +
-          " " +
-          result.recordset[i].price +
-          " " +
-          result.recordset[i].description +
-          " " +
-          result.recordset[i].picture +
-          "<br>";
+      function createTable(result) {
+        for (let i=0; i < result.recordset.length; i++) {
+          var table = document.getElementById("table");
+          var row = table.insertRow(i);
+
+          var cell1 = row.insertCell(0);
+          var cell2 = row.insertCell(1);
+          var cell3 = row.insertCell(2);
+          var cell4 = row.insertCell(3);
+          var cell5 = row.insertCell(4);
+          var cell6 = row.insertCell(5);
+          var cell7 = row.insertCell(6);
+
+
+          cell1.innerHTML = result.recordset[i].email;
+          cell2.innerHTML = result.recordset[i].postal;
+          cell3.innerHTML = result.recordset[i].category_name;
+          cell4.innerHTML = result.recordset[i].created_at;
+          cell5.innerHTML = result.recordset[i].price;
+          cell6.innerHTML = result.recordset[i].description;
+          cell7.innerHTML = result.recordset[i].picture;
+
+          table.appendChild(row);
       }
-      console.log(posts);
-      document.getElementById("Container").innerHTML = posts;
     }
-  });
-});
+  })
+})
