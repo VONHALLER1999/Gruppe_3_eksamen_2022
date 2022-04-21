f5de201e2ca1c6c27ddc7181946b0e448128067a
Madswivel <madswivel@live.dk>
2022-04-21 15:37:42 +0200

    unitTest.js Finnish
    sidste rettelser til krav 9 + fjernelse af ekstra API test ifb med session og krav

--- unitTest/unitTest.js
+++ unitTest/unitTest.js
@@ -4,7 +4,10 @@ const chaiHttp = require("chai-http");
 const app = require("../eksamen/app.js");
 
 chai.use(chaiHttp);
-var agent = chai.request.agent(app)
+var agent = chai.request.agent(app);
+
+//open integreret terminal -> run: "npm test "
+//test: "mocha --timeout 10000 -- unitTest" (forlænget timeout)
 
 //Krav 9 testes her
 describe("Unittest af Krav 4", () => {  
@@ -13,14 +16,15 @@ describe("Unittest af Krav 4", () => {
       agent
         .post('/login')
         .send({ email: 'test@1', password: 'test' })
-        .end((err, res, result) => { //result == null
+        .end((err, res) => { 
           expect(err).to.be.null;
           expect(res.status).to.equal(200);
-          //indsæt ny
+          expect(res.body).to.equal(true);
           done();     
         });
     });
   });
+
   describe("POST /login", () => {                 
     it("logger falsk testbruger", (done) => {
       agent
@@ -29,25 +33,23 @@ describe("Unittest af Krav 4", () => {
         .end((err, res) => {
           expect(err).to.be.null;
           expect(res.status).to.equal(200);
-          //indsæt ny
+          expect(res.body).to.equal(false);
           done();     
         });
     });
   });
-  describe("GET /loggedstatus", () => {  
-    it("kontrollere loggedstatus", (done) =>{
+
+  describe("POST /login", () => {                 
+    it("logger ingen testbruger", (done) => {
       agent
-      .post('/login')
-      .send({ email: 'test@1', password: 'test' })
-      .then(function(res){
-        agent.get('/loggedstatus')
-             .end((err, res) => {
-               expect(err).to.be.null;
-                expect(res.status).to.equal(200);
-                //indsæt ny
-                done();
-             });
-      });
+        .post('/login')
+        .send({ email: '', password: '' })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res.status).to.equal(200);
+          expect(res.body).to.equal(false);
+          done();     
+        });
     });
   });
 });
